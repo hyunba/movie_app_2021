515be8c5648b26a930821e0b2af0543cf97152f6
unknown <dls272@naver.com>
2021-10-08 19:10:56 +0900

    #2.0 PropTypes React App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Food({ name }) {
+function Food({ name, rating }) {
   return(
+  <div>
     <h3>I love {name}</h3>
+    <h2>{rating} / 5</h2>
+  </div>
+  );
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  rating: PropTypes.number
+}
+
+const foodILike = [
+  {
+    name: "Kimchi",
+    rating: 5.0
+  },
+  {
+    name: "Samgyeopsal",
+    rating: 4.9
+  },
+  {
+    name: "Bibimbap",
+    rating: 4.2
+  },
+  {
+    name: "Doncasu",
+    rating: 5.0
+  }
+];
+
+function renderFood(dish) {
+  return(
+    <Food name = {dish.name} rating = {dish.rating} />
   );
 }
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-      <Food name = "potato" />
-      <Food name = "hamburger" />
-      <Food name = "pizza" />
-      <Food name = "chicken" />
+      {foodILike.map(renderFood)}
     </div>
   );
       
