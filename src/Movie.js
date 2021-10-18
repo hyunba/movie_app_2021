ca11c4796f972e7e27f30a7e789e442fdd22e037
unknown <dls272@naver.com>
2021-10-19 01:29:21 +0900

    #4.1 style React App

--- src/Movie.js
+++ src/Movie.js
@@ -1,10 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
 // 앞에서 한 App.js에서는 state가 필요해서 class component를 해주었지만
 // Movie.js에서는 state가 필요하지 않아 function component를 해준다.
-function Movie({id, year, title, summary, poster}){ // propTypes에서 정보를 가져와서 movie component에 준비한다
-    return <h4>{title}</h4>;
+function Movie({year, title, summary, poster}){ // propTypes에서 정보를 가져와서 movie component에 준비한다
+    // class에 style을 주고 싶다면, style={{backgroundColor: "red"}}  이런 식으로 주면된다.
+    // 다른 방식으로는 css 파일을 만들면된다.
+    return (
+        <div class="movie"> 
+            <img src={poster} alt={title} title={title} />
+            <div class="movie__data">
+                <h3 class="movie__title">{title}</h3>
+                <h5 class="movie__year">{year}</h5>
+                <p class="movie__summary">{summary}</p>
+            </div>
+        </div>
+    );
 }
 
 // 우리가 얻어올 prop을 찾는다
